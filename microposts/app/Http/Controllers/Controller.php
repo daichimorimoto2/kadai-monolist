adf5fdde63054aa62c034d76c71cc2159f0cebdf
jiro.kirameki <ec2-user@ip-172-31-1-118.us-east-2.compute.internal>
2018-06-05 03:53:56 +0000

    init

--- microposts/app/Http/Controllers/Controller.php
+++ microposts/app/Http/Controllers/Controller.php
@@ -7,17 +7,7 @@ use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
-
-
-abstract class Controller extends BaseController
+class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
-
-    public function counts($user) {
-        $count_microposts = $user->microposts()->count();
-
-        return [
-            'count_microposts' => $count_microposts,
-        ];
-    }
 }
--- microposts/config/app.php
+++ microposts/config/app.php
@@ -65,7 +65,7 @@ return [
     |
     */
 
-    'timezone' => 'Asia/Tokyo',
+    'timezone' => 'UTC',
 
     /*
     |--------------------------------------------------------------------------
@@ -226,8 +226,6 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
 
-        'Form' => Collective\Html\FormFacade::class,
-        'Html' => Collective\Html\HtmlFacade::class,
     ],
 
 ];
